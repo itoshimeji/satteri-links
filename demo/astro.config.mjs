eb6441690429a322b22dcb25feab41266e363683
ItoShimeji <hamazakidaiti@gmail.com>
2026-08-07 16:02:01 +0900

    feat: demo site

---
+++ demo/astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import mdx from "@astrojs/mdx";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "static",
+  integrations: [mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: "github-dark",
+    },
+  },
+});
